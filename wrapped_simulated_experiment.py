caef938cac6ccfeb76cad2d7933db6c735296793
Your Name <you@example.com>
2023-08-07 16:57:31 +0100

    comparison methods and hyperparameters

--- wrapped_simulated_experiment.py
+++ wrapped_simulated_experiment.py
@@ -27,6 +27,7 @@ from sklearn.neural_network import MLPClassifier
 import numpy as np
 from datetime import datetime
 from src.utils import *
+from src.comparison_models import tmle_test,double_ml_test
 
 def update_cfg(cfg, value):
     if cfg["moving_param"]["beta_scalar"]:
@@ -42,14 +43,14 @@ def construct_dir_name(cfg, value):
         return "beta_scalar="+str(value)
     if cfg["moving_param"]["n_train_sample"]:
         return "n_sample="+str(value)
-
+    
 weights_model_dict = {"Logistic_Regression": LogisticRegression(), "Adaboost":AdaBoostClassifier(),"Decision_Tree":DecisionTreeClassifier(),"GP":GaussianProcessClassifier(),"MLP":MLPClassifier()}
 kernel_dict = {"RBF" : kernel.RBFKernel}
+comparison_model_dict = {"DML":double_ml_test , "TMLE":tmle_test}
 
-def main(args, cfg,direct_path):
+def main(args, cfg,result_dict):
 
     weights_model = weights_model_dict[cfg["experiment"]["weights_model"]]
-    results_dict = {}
 
     X_ker = kernel_dict[cfg["experiment"]["X_ker"]]()
     Y_ker = kernel_dict[cfg["experiment"]["Y_ker"]]()
@@ -63,34 +64,44 @@ def main(args, cfg,direct_path):
         print("Not Permuting")
     cme_reg = cfg["experiment"]["cme_reg"]
 
+    data_train,data_val = make_data(cfg)
+    data_full = data_train.join(data_val)
+    X_ker.lengthscale = get_median_ls(data_full.X)
+    Y_ker.lengthscale = get_median_ls(data_full.Y)
     if type(cme_reg) is list:
         cross_val = True
-        reg_param_range = np.linspace(cme_reg[0],cme_reg[1],num=cme_reg[2]) 
-        data_train,data_val = make_data(cfg) 
-        cme_reg = cme_cross_validate(data_train,data_val,X_ker,Y_ker,reg_param_range)
-        print(cme_reg)
+        reg_param_range = np.linspace(cme_reg[0],cme_reg[1],num=cme_reg[2])  
+        cme_reg_0 = cme_cross_validate_weighted(data_train,data_val,X_ker,Y_ker,reg_param_range,T_val=0)
+        cme_reg_1 = cme_cross_validate_weighted(data_train,data_val,X_ker,Y_ker,reg_param_range,T_val=1)
+        cme_reg = [cme_reg_0,cme_reg_1]
     else: 
         cross_val = False
+        cme_reg = [cme_reg,cme_reg]
 
     for i in tqdm.tqdm(range(cfg["experiment"]["n_iter"])):
         data_train,data_test = make_data(cfg)
+        data_full = data_train.join(data_test)
         weights_model.fit(X= data_train.X, y=data_train.T)
         
         for stat in cfg["experiment"]["test_stat"]:
             for func in cfg["experiment"]["ker_regress"]:
                 result = kernel_permutation_test(data_train,data_test,X_ker,Y_ker,weights_model,test_stat=stat,n_bins =n_bins,permute_weights=permute_weights , reg=cme_reg,func = func,KMM_weights = KMM_weights)
-                results_dict[stat+func] = results_dict.get(stat+func,0) + float((result["p_val"] < cfg["experiment"]["significance_level"]))
-    for stat in cfg["experiment"]["test_stat"]:
-            for func in cfg["experiment"]["ker_regress"]:
-                results_dict[stat+func] = results_dict[stat+func]/cfg["experiment"]["n_iter"]
-                results_dict[stat+func+"CI"] = get_confidence_interval(results_dict[stat+func],cfg["experiment"]["n_iter"])
-    # Dump scores
-    if cross_val:
-        results_dict["cme_reg"] = cme_reg.item()
-    dump_path = os.path.join(direct_path, 'scores.metrics')
-    with open(dump_path, 'w') as f:
-        yaml.dump(results_dict, f)
-    logging.info(f"\n Dumped scores at {direct_path}")
+                result_dict["test_stat"] += [stat+func]
+                result_dict["p_val"] += [result["p_val"].item()]
+                if cfg["moving_param"]["beta_scalar"]:
+                    result_dict["beta_scalar"] += [cfg["data"]["arguments"]["beta_scalar"]]
+                if cfg["moving_param"]["n_train_sample"]:
+                    result_dict["n_sample"] += [cfg["data"]["n_train_sample"]]
+        
+        for model in cfg["experiment"]["comparison_model"]:
+            result = comparison_model_dict[model](data_full)
+            result_dict["test_stat"] += [model]
+            result_dict["p_val"] += [result.item()]
+            if cfg["moving_param"]["beta_scalar"]:
+                result_dict["beta_scalar"] += [cfg["data"]["arguments"]["beta_scalar"]]
+            if cfg["moving_param"]["n_train_sample"]:
+                result_dict["n_sample"] += [cfg["data"]["n_train_sample"]]
+
     return weights_model
 
 def make_data(cfg):
@@ -122,20 +133,22 @@ if __name__ == "__main__":
 
     now = datetime.now()
     date_time_str = now.strftime("%m-%d %H:%M:%S")
+    result_dict = {"test_stat":[], "p_val":[]}
+
+    if cfg["moving_param"]["beta_scalar"]:
+        result_dict["beta_scalar"] = []
+    if cfg["moving_param"]["n_train_sample"]:
+        result_dict["n_sample"] = []
     direct_path = os.path.join(args['--o'],date_time_str)
     # Create output directory if doesn't exists
     os.makedirs(direct_path, exist_ok=True)
     with open(os.path.join(direct_path, 'cfg.yaml'), 'w') as f:
         yaml.dump(cfg, f)
-    os.chdir(direct_path)
     for value in cfg["moving_param"]["values"]:
         cfg = update_cfg(cfg, value) 
-        direct_path = construct_dir_name(cfg,value)
-        os.makedirs(direct_path, exist_ok=True)   
-        weights_model = main(args, cfg,direct_path)
+        weights_model = main(args, cfg,result_dict)
         if args["--plot"]:
             function_dict = {}
-
             if cfg["data"]["generator"] == "shift_data_simulation":
                 for key in cfg["data"]["functions"]:
                     function_dict[key] = getattr(data,cfg["data"]["functions"][key])
@@ -148,4 +161,8 @@ if __name__ == "__main__":
                 data_plot = data.linear_data_simulation(n_sample=1000,**cfg["data"]["arguments"])
                 data_plot.save_data_plot(direct_path)
                 save_plot_weights_hist(weights_model, data_plot,direct_path)
+    dump_path = os.path.join(direct_path, 'scores.metrics')
+    with open(dump_path, 'w') as f:
+        yaml.dump(result_dict, f)
+    logging.info(f"\n Dumped scores at {direct_path}")
     # Run session
